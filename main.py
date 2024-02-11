239e2084196a89b9a26e5f53bd90495d0c45df3c
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-02-11 09:01:32 +0500

    Add multithreading support for handling multiple clients

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import socket
+import threading
+
+
+def handleClient(conn: socket, addr):
+    with conn:
+        print(f"Connected to {addr}")
+        while True:
+            message = conn.recv(1024).decode()
+            if not message:
+                print(f"Disconnected from {addr}")
+                break
+            print(f"Received a message from {addr}: {message}")
 
 
 def main():
     with socket.create_server(("localhost", 6379), reuse_port=True) as server:
+        server.listen()
         while True:
-            server.listen()
-            conn, addr = server.accept()
-            with conn:
-                print(f"Connected to {addr}")
-                while True:
-                    message = conn.recv(1024).decode()
-                    if not message:
-                        print("Disconnected")
-                        break
-                    print(f"Received this message: {message}")
+            try:
+                conn, addr = server.accept()
+            except Exception as e:
+                print(f"Error occured: {e}")
+            clientThread = threading.Thread(
+                target=handleClient, args=(conn, addr), daemon=True
+            )
+            clientThread.start()
 
 
 if __name__ == "__main__":
