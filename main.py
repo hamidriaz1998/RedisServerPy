db1c5f387921951df2542a395143f54264602bb1
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-02-11 10:54:42 +0500

    Add functionality to parse PING and ECHO commands

--- main.py
+++ main.py
@@ -2,15 +2,55 @@ import socket
 import threading
 
 
+def parseRequest(request: str):
+    """
+    Parse the request string and extract the command and its arguments.
+
+    Args:
+        request (str): The request string.
+
+    Returns:
+        Tuple[str, list[str]]: A tuple containing the command and its arguments.
+    """
+    if request.startswith("*"):  # Bulk arrays
+        parts = request.split("\r\n")
+        command = parts[2].upper()
+        args = parts[4:-1:2]
+        return command, args
+    else:
+        raise ValueError("Invalid command format")
+
+
+def generateResponse(command: str, args: list[str]):
+    """
+    Generate the response string based on the command and its arguments.
+
+    Args:
+        command (str): The command.
+        args (list[str]): The arguments.
+
+    Returns:
+        str: The response string.
+    """
+    if command == "ECHO" or (command == "PING" and args):
+        return "+" + " ".join(args) + "\r\n"
+    elif command == "PING" and not args:
+        return "+PONG\r\n"
+    else:
+        raise ValueError("Unsupported command")
+
+
 def handleClient(conn: socket, addr):
     with conn:
         print(f"Connected to {addr}")
         while True:
-            message = conn.recv(1024).decode()
-            if not message:
+            request = conn.recv(1024).decode()
+            if not request:
                 print(f"Disconnected from {addr}")
                 break
-            print(f"Received a message from {addr}: {message}")
+            command, args = parseRequest(request)
+            response = generateResponse(command, args).encode()
+            conn.sendall(response)
 
 
 def main():
